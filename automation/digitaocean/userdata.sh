350a38b62da61426e6406a6d7f0b965659ded5e6
i-brzhv <i-brzhv@protonmail.com>
2022-03-03 00:40:50 +0200

    go

--- automation/digitaocean/userdata.sh
+++ automation/digitaocean/userdata.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
 apt -y update
-apt -y install docker.io
+apt -y install docker.io git
 
 fallocate -l 1G /swapfile && chmod 600 /swapfile && mkswap /swapfile && swapon /swapfile 
+
+git clone https://github.com/brzhv-cyb/stop-russia.git
+
+cd stop-russia && docker build -t bomb .
 
 NUMPROC=5
 
 for proc in $(seq 1 $NUMPROC); do
-  screen -d -m docker run -ti --rm abagayev/stop-russia
+  screen -d -m docker run -ti --rm bomb
 done
