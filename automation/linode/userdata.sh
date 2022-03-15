da65a177faa1c7ce3e089d9619adef49d70f6786
ibrzhv <i-brzhv@protonmail.com>
2022-03-15 20:14:14 +0200

    go

--- automation/linode/userdata.sh
+++ automation/linode/userdata.sh
@@ -5,4 +5,4 @@ apt -y install docker.io git
 
 fallocate -l 1G /swapfile && chmod 600 /swapfile && mkswap /swapfile && swapon /swapfile 
 
-screen -d -m docker run -it --rm alexmon1989/dripper:latest -s ${TARGET} -p 53 -t 5 -m udp -l 2048
+screen -d -m docker run -it --rm alexmon1989/dripper:latest -s ${target} -p 53 -t 5 -m udp -l 2048
